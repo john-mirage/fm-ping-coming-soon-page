ae5b74d6e194d3ac8c7a84038bfef278b3c3a16d
john-mirage <chaosdynamix@posteo.net>
2022-10-13 18:19:54 +0200

    feat: setup files for project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,7 +14,12 @@ module.exports = {
       "hover-device": { "raw": "(hover: hover)" },
     },
     colors: {
-      transparent: "transparent",
+      "blue": "hsl(223, 87%, 63%)",
+      "pale-blue": "hsl(223, 100%, 88%)",
+      "light-red": "hsl(354, 100%, 66%)",
+      "gray": "hsl(0, 0%, 59%)",
+      "very-dark-blue": "hsl(209, 33%, 12%)",
+      "transparent": "transparent",
     },
     spacing: {
       "0": "0px",
@@ -63,7 +68,7 @@ module.exports = {
     }),
     extend: {
       fontFamily: {
-        sans: ["font name", ...defaultTheme.fontFamily.sans],
+        sans: ["Libre Franklin", ...defaultTheme.fontFamily.sans],
       },
     },
   },
