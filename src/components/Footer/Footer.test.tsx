380edb1d0535e4ac0e802ec800fbf114d516a6d5
john-mirage <chaosdynamix@posteo.net>
2022-10-16 04:05:46 +0200

    feat: test footer icons

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 
 describe("Footer component", () => {
   it("should display the footer nav links", () => {
-    render(<Footer />);
+    const { container } = render(<Footer />);
     const facebookLink = screen.getByLabelText(/Go to the Ping facebook page/i);
     const twitterLink = screen.getByLabelText(/Go to the Ping twitter page/i);
     const instagramLink = screen.getByLabelText(
@@ -16,6 +16,9 @@ describe("Footer component", () => {
     expect(facebookLink).toHaveAttribute("href", "#");
     expect(twitterLink).toHaveAttribute("href", "#");
     expect(instagramLink).toHaveAttribute("href", "#");
+    expect(container.querySelector('use[href="#icon-facebook"]')).toBeTruthy();
+    expect(container.querySelector('use[href="#icon-twitter"]')).toBeTruthy();
+    expect(container.querySelector('use[href="#icon-instagram"]')).toBeTruthy();
   });
 
   it("should display the footer copyright", () => {
