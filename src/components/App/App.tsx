e371d69a6cffd8ea63ee380e775ff04f268ad609
john-mirage <chaosdynamix@posteo.net>
2022-10-20 00:44:05 +0200

    fix: increase bottom margin of the main section

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="container mx-auto flex min-h-screen flex-col items-center justify-start px-24 pt-84 pb-36 lg:pb-48">
       <Header className="mb-40 lg:mb-48" />
-      <Main className="mb-32" />
+      <Main className="mb-64" />
       <Footer className="mt-auto" />
     </div>
   );
